6baec41420c518d1d13f91671f192313fc5dfe50
menna5252 <menna.route@gmail.com>
2025-09-16 03:03:12 +0300

    thir2d commit

--- src/app/components/forms/Loginform.tsx
+++ src/app/components/forms/Loginform.tsx
@@ -58,7 +58,7 @@ export default function LoginForm() {
             toast.success('Welcome Back',{
                     position:'top-center'
                   }) 
-                  router.push('/')
+                  router.push(res?.url||'/')
                    
         }
         else{
