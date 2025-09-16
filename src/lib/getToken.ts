d0f32c084b36f176a5abe6d55f618a81058dce0e
menna5252 <menna.route@gmail.com>
2025-09-16 03:19:44 +0300

    final commit

--- src/lib/getToken.ts
+++ src/lib/getToken.ts
@@ -4,7 +4,9 @@ import { cookies } from "next/headers";
 
 
 export async  function getAuthToken(){
-const authToken = (await cookies()).get('next-auth.session-token')?.value;
+const cookieToken = await cookies();
+
+const authToken= cookieToken.get('next-auth.session-token')?.value||cookieToken.get('__Secure-next-auth.session-token')?.value;
 const decodedToken = await decode({token:authToken,secret:process.env.NEXTAUTH_SECRET!})
 console.log('tokendddd',decodedToken?.token)
 
